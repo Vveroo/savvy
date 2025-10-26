6e205819931bf1f02f8bb041f855eaef113be930
sophiarb09 <sophiarb2009@gmail.com>
2025-10-25 23:02:19 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { UserProvider } from './contexts/UserContext';
 import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import CardapioScreen from './screens/CardapioScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <UserProvider>
-      <LoginScreen />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Cardapio" component={CardapioScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </UserProvider>
   );
-};
+}
