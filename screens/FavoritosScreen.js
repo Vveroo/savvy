2c474117650265ac216b7ab56d416476a3f997dc
sophiarb09 <sophiarb2009@gmail.com>
2025-11-03 21:23:54 -0300

    .

--- screens/FavoritosScreen.js
+++ screens/FavoritosScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList} from 'react-native';
+import { styles } from '../styles/favoritoStyles';
 
 export default function FavoritosScreen() {
   return (
-    <View>
-      <Text>Favoritos</Text>
+    <View style={styles.container}>
+    <Text>Favoritos</Text>
     </View>
   );
 }
