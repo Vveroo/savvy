fbe5cc3caac11c932882aee185cd4df5df80c54f
caligula <mirytv8@gmail.com>
2025-12-02 10:20:27 -0300

    asd

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -70,7 +70,7 @@ export default function CartScreen({ navigation }) {
         { 
           text: "OK", 
           onPress: () => {
-            navigation.navigate("MeusPedidos");
+            navigation.navigate("Pedidos");
           } 
         }
       ]);
